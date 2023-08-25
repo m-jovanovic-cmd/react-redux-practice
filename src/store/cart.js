d3a9de8ccf833ebaf8de541b8be807ac52f27071
Mladen <mladenjovanovic95@hotmail.com>
2023-08-25 18:54:46 +0200

    Something

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,13 +1,52 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { showCart: true, items: [], totalQuantity: 0 }
+import { uiActions } from "./ui";
+
+const initialShit = 
+
+export const fetchCartData = () => {
+    return async (dispatch) => {
+        const sendFirst = async () => {
+            await fetch()
+        }
+        const fetchData = async () => {
+            const response = await fetch('https://custom-react-hooks-chapter15-default-rtdb.europe-west1.firebasedatabase.app/cart.json', { 
+                method: 'PUT',
+                body: JSON.stringify(initialShit),
+            });
+
+            if (!response.ok) {
+                throw new Error("Could not fetch cart data!");
+            }
+
+            const data = await response.json();
+            return data;
+        };
+
+        try {
+            const cartData = await fetchData();
+            dispatch(cartActions.replaceCart(cartData));
+
+        } catch(error) {
+            dispatch(uiActions.showNotification({
+                status: 'error',
+                title: 'Error!',
+                message: 'Fetching cart data failed!'
+            }));
+        }
+    };
+};
 
 const cartSlice = createSlice({
     name: 'cart',
-    initialState,
+    initialState: { 
+        items: [], 
+        totalQuantity: 0 
+    },
     reducers: {
-        toggleCart(state) {
-            state.showCart = !state.showCart;
+        replaceCart(state, action) {
+            state.items = action.payload.items;
+            state.totalQuantity = action.payload.totalQuantity;
         },
         addItem(state, action) {
             const itemIndex = state.items.findIndex((item) => item.id === action.payload.id);
@@ -29,6 +68,44 @@ const cartSlice = createSlice({
         }
     }
 });
+
+export const sendCartData = (cart) => {
+    return async (dispatch) => {
+        dispatch(uiActions.showNotification({
+            status: 'pending',
+            title: 'Sending...',
+            message: 'Sending cart data!'
+        }));
+
+        const sendRequest = async () => {
+            const response = await fetch('https://custom-react-hooks-chapter15-default-rtdb.europe-west1.firebasedatabase.app/cart.json', { 
+                method: 'PUT',
+                body: JSON.stringify(cart),
+            });
+    
+            if (!response.ok) {
+                throw new Error('Error encountered!');
+            }
+        };
+
+        try {
+            await sendRequest();
+
+            dispatch(uiActions.showNotification({
+                status: 'success',
+                title: 'Success!',
+                message: 'Send cart data successfully!'
+            }));
+        } catch (error) {
+            dispatch(uiActions.showNotification({
+                status: 'error',
+                title: 'Error!',
+                message: 'Sending cart data failed!'
+            }));
+        };
+    };
+};
+
 
 export const cartActions = cartSlice.actions;
 export default cartSlice.reducer;
